68bd2e1a2bf0d0b8fb3fe6ab7696dd3be41a3902
Alejandrohp96 <alejandroherrepes@gmail.com>
2026-03-26 19:19:03 +0000

    Actualización

--- src/components/home/CanonCosmoHome.tsx
+++ src/components/home/CanonCosmoHome.tsx
@@ -1,6 +1,10 @@
 const CanonCosmoHome = () => {
   return (
-    <div>CanonCosmoHome</div>
+    <div>
+      <div className="bg-[#0001AC] p-4">
+        <h1 className="text-2xl font-bold">Welcome to Canon Cosmo Home!</h1>
+      </div>
+    </div>
   )
 }
 
